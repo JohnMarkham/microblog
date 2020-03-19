964a438979c4a2fcba64c54730bd4f163c3096dc
JohnMarkham <seanomarachain@gmail.com>
2020-03-19 23:59:30 +0000

    new route

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,28 @@
-from app import app
+from flask import render_template, flash, redirect
+from app import my_app
+from app.forms import LoginForm
 
-@app.route('/')
-@app.route('/index')
+#This is the URL /lgoinsss, and login is the name referened in url_for('login')
+@my_app.route('/loginsss', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, rembemer_me={}'.format(form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title = 'Sign In', form = form)
+
+@my_app.route('/')
+@my_app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {"username":"John Markham"}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool'
+        }
+    ]
+    return render_template('index.html', title='Home', user = user, posts = posts)
