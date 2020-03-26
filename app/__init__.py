166ea35f2c6a539b146a2a634b1b6d62b481e0ff
JohnMarkham <seanomarachain@gmail.com>
2020-03-26 12:19:21 +0000

    next set of changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,44 @@
+from flask_login import LoginManager
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
 
 my_app = Flask(__name__)
 my_app.config.from_object(Config)
 db = SQLAlchemy(my_app)
 migrate = Migrate(my_app, db)
+login = LoginManager(my_app)
+login.login_view = 'login'
 
-from app import routes, models
+from app import routes, models, errors
+
+
+if not my_app.debug:
+    if my_app.config['MAIL_SERVER']:
+        auth = None
+        if my_app.config['MAIL_USERNAME'] or my_app.config['MAIL_PASSWORD']:
+            auth = (my_app.config['MAIL_USERNAME'], my_app.config['MAIL_PASSWORD'])
+        secure = None
+        if my_app.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(my_app.config['MAIL_SERVER'], my_app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + my_app.config['MAIL_SERVER'],
+            toaddrs=my_app.config['ADMINS'], subject='Microblog Failure',
+            credentials=auth, secure = secure)
+        mail_handler.setLevel(logging.ERROR)
+        my_app.logger.addHandler(mail_handler)
+
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message) s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    my_app.logger.addHandler(file_handler)
+
+    my_app.logger.setLevel(logging.INFO)
+    my_app.logger.info('microblog startup')
--- app/models.py
+++ app/models.py
@@ -1,8 +1,17 @@
 from werkzeug.security import check_password_hash, generate_password_hash
 from datetime import datetime
-from app import db
+from app import db, login
+from flask_login import UserMixin
+from hashlib import md5
 
-class User(db.Model):
+
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id'))
+    )
+
+
+class User(db.Model, UserMixin):
     '''User Class'''
 
     '''These are Class variables defined here because that's how SQLAlchemy knows the table structure'''
@@ -10,14 +19,27 @@ class User(db.Model):
     email = db.Column(db.String(120), index = True, unique = True)
     username = db.Column(db.String(64), index = True, unique = True)
     password_hash = db.Column(db.String(128))
-    posts = db.relationship('Post', backref='author', lazy = 'dynamic')
+    posts = db.relationship('Post', backref='XXXXXXXXXXXXXX', lazy = 'dynamic')
+    about_me = db.Column(db.String(256))
+    last_seen = db.Column(db.DateTime, default = datetime.utcnow)
 
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin = (followers.c.follower_id == id),
+        secondaryjoin = (followers.c.follower_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic'
+        )
 
-
-    def __init__(self, username = None, email = None, posts = None):
+    '''
+    def __init__(self, username):
         self.username = username
-        self.email = email
-        self.posts = posts
+        #self.email = email
+        #self.posts = posts
+    '''
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -26,7 +48,25 @@ class User(db.Model):
         return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
-        return '<User {}>'.format(self.username)
+        return '<User {} >'.format(self.username)
+
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.appear(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):
+        return self.followed.filter(followers.c.followed_id == user.id).count() > 0
+
+    def followed_posts(self):
+        followed = Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(followers.c.follower_id == self.id)
+        )
+        own = Post.query.filter_by(user_id = self.id)
+        return followed.union(own).order_by(Post.timestamp.desc())
 
 
 class Post(db.Model):
@@ -37,3 +77,10 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+
+@login.user_loader
+def load_user(id):
+    '''Because Flask-Login knows nothing about databases,
+    it needs the application's help in loading a user.'''
+    return User.query.get(int(id))
